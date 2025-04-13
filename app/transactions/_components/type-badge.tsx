d3467a0501ce0e8cc3db61f889a603941fd9ea90
Luiz Gustavo <luizgustavo974@hotmail.com>
2025-04-13 18:08:57 -0300

    feat: ensure transaction table labels are rendered properly

--- app/transactions/_components/type-badge.tsx
+++ app/transactions/_components/type-badge.tsx
@@ -17,14 +17,14 @@ const TransactionTypeBadge = ({ transaction }: TransactionTypeBadgeProps) => {
   }
   if (transaction.type === TransactionType.EXPENSE) {
     return (
-      <Badge className="bg-danger bg-opacity-10 font-bold text-danger">
+      <Badge className="bg-danger bg-opacity-10 font-bold text-danger hover:bg-muted">
         <CircleIcon className="mr-2 fill-danger" size={10} />
         Despesa
       </Badge>
     );
   }
   return (
-    <Badge className="bg-white bg-opacity-10 font-bold text-white">
+    <Badge className="bg-white bg-opacity-10 font-bold text-white hover:bg-muted">
       <CircleIcon className="mr-2 fill-white" size={10} />
       investimento
     </Badge>
