f64260ccd39e8abdb8a88d94bfca436ed3f86b14
Luiz Gustavo <luizgustavo974@hotmail.com>
2025-07-13 20:47:46 -0300

    feat: add transactions pie chart component

--- app/(home)/_components/summary-cards.tsx
+++ app/(home)/_components/summary-cards.tsx
@@ -5,64 +5,21 @@ import {
   WalletIcon,
 } from "lucide-react";
 import SummaryCard from "./summary-card";
-import { db } from "@/app/_lib/prisma";
 
 interface SummaryCardsProps {
   month: string;
+  balance: number;
+  depositsTotal: number;
+  investimentsTotal: number;
+  expensesTotal: number;
 }
 
-const SummaryCards = async ({ month }: SummaryCardsProps) => {
-  const where = {
-    date: {
-      gte: new Date(`2025-${month}-01`),
-      lt: new Date(`2025-${month}-31`),
-    },
-  };
-
-  const depositsTotal = Number(
-    (
-      await db.transaction.aggregate({
-        where: {
-          ...where,
-          type: "DEPOSIT",
-        },
-        _sum: {
-          amount: true,
-        },
-      })
-    )?._sum?.amount,
-  );
-
-  const investimentsTotal = Number(
-    (
-      await db.transaction.aggregate({
-        where: {
-          ...where,
-          type: "INVESTMENT",
-        },
-        _sum: {
-          amount: true,
-        },
-      })
-    )?._sum?.amount,
-  );
-
-  const expensesTotal = Number(
-    (
-      await db.transaction.aggregate({
-        where: {
-          ...where,
-          type: "EXPENSE",
-        },
-        _sum: {
-          amount: true,
-        },
-      })
-    )?._sum?.amount,
-  );
-
-  const balance = depositsTotal - investimentsTotal - expensesTotal;
-
+const SummaryCards = async ({
+  balance,
+  depositsTotal,
+  investimentsTotal,
+  expensesTotal,
+}: SummaryCardsProps) => {
   return (
     <div className="space-y-6">
       <SummaryCard
